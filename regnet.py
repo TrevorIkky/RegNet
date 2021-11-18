2f9f1f72a22ec318034f43b9a85427c704217975
ikky <ikramikky4462@gmail.com>
2021-11-18 14:38:20 +0300

    Added new arg for rnn cell type

--- regnet.py
+++ regnet.py
@@ -16,7 +16,7 @@ max_epochs = 10
 batch_size = 32
 
 class rnn_regulated_block(nn.Module):
-    def __init__(self, in_channels, intermediate_channels, conv_lstm, identity_block=None, stride=1):
+    def __init__(self, in_channels, intermediate_channels, rnn_cell, identity_block=None, stride=1):
         super(rnn_regulated_block, self).__init__()
         #print(f'In channels {in_channels} | Intermediate channels: {intermediate_channels} ')
         self.identity_block = identity_block
@@ -24,7 +24,7 @@ class rnn_regulated_block(nn.Module):
         self.bn1 = nn.BatchNorm2d(intermediate_channels)
         self.relu = nn.ReLU()
 
-        self.conv_lstm = conv_lstm
+        self.rnn_cell = rnn_cell
         self.bn2 = nn.BatchNorm2d(intermediate_channels)
         self.conv2 = nn.Conv2d(intermediate_channels, intermediate_channels, kernel_size=3, padding=1, stride=1)
         self.bn2 = nn.BatchNorm2d(intermediate_channels)
@@ -34,16 +34,17 @@ class rnn_regulated_block(nn.Module):
 
         self.conv4 = nn.Conv2d(intermediate_channels, intermediate_channels * 4, kernel_size=1, stride=1)
         self.bn4 = nn.BatchNorm2d(intermediate_channels * 4)
-    def forward(self, x:torch.Tensor, state:torch.Tensor) -> typing.Tuple:
+    def forward(self, x:torch.Tensor, state:typing.Tuple) -> typing.Tuple:
         y = x.clone()
-
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
 
-        #print(f'x.shape {x.shape} h.shape {state[1].shape}')
+        if isinstance(self.rnn_cell, ConvLSTMCell):
+            c, h = self.rnn_cell(x, state)
+        else:
+            c = None; h = self.rnn_cell(x, state[1])
 
-        c, h = self.conv_lstm(x, state)
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
@@ -61,10 +62,10 @@ class rnn_regulated_block(nn.Module):
 
         if self.identity_block is not None:
             x += self.identity_block(y)
-        return h, self.relu(x)
+        return c, h, self.relu(x)
 
 class RegNet(pl.LightningModule):
-    def __init__(self, in_dim:int, classes:int=3, layers:typing.List=[3, 4, 6, 3]):
+    def __init__(self, in_dim:int, classes:int=3, cell_type:str='gru', layers:typing.List=[3, 4, 6, 3]):
         super(RegNet, self).__init__()
         self.layers = layers
         self.classes = classes
@@ -73,12 +74,12 @@ class RegNet(pl.LightningModule):
         self.bn1 = nn.BatchNorm2d(self.intermediate_channels)
         self.relu = nn.ReLU()
         self.max_pool = nn.MaxPool2d((3, 3) , padding=1, stride=2)
+        self.cell = ConvGRUCell if cell_type == 'gru' else ConvLSTMCell
         regulated_blocks = []
         for layer in range(len(layers)):
             stride = 1 if layer < 1 else 2
             channels = self.intermediate_channels if layer < 1 else self.intermediate_channels // 2
             h_channels = 64
-            #print(f'Layer: {layer} | intermediate: {self.intermediate_channels} | channels: {channels} | h_channels: {h_channels}')
             identity_block = None
             if layer > 1:
                 identity_block = nn.Sequential(
@@ -88,19 +89,17 @@ class RegNet(pl.LightningModule):
 
             regulated_blocks.append(rnn_regulated_block(
                 self.intermediate_channels, channels,
-                ConvLSTMCell(channels,h_channels , kernel_size=3),
+                self.cell(channels,h_channels , kernel_size=3),
                 identity_block, stride
             ))
 
             for block in range(layers[layer] - 1):
                 self.intermediate_channels = channels * 4 if block < 1 else self.intermediate_channels
-                #print(f'Layer: {layer} | Block: {block} | intermediate: {self.intermediate_channels} | channels: {channels} | h channels {h_channels}')
-                conv_lstm = ConvLSTMCell(channels, h_channels , kernel_size=3)
+                conv_lstm = self.cell(channels, h_channels , kernel_size=3)
                 regulated_blocks.append(rnn_regulated_block(
                         self.intermediate_channels, channels, conv_lstm
                     )
                 )
-            #print(f'===================End Layer {layer} =====================')
 
         self.state_avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
         self.regulated_blocks = nn.ModuleList(regulated_blocks)
@@ -113,12 +112,15 @@ class RegNet(pl.LightningModule):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.max_pool(x)
-        h = torch.zeros(x.shape)
+        c, h = torch.zeros(x.shape), torch.zeros(x.shape)
         for i, block in enumerate(self.regulated_blocks):
             #print(f'Block: {i}, x.shape: {x.shape}, h.shape {h.shape}')
-            h, x = block(x, (h, h))
+            c, h, x = block(x, (c, h))
             if h.shape[-1] != x.shape[-1]:
-               h = self.state_avg_pool(h)
+                h = self.state_avg_pool(h)
+                if c is not None:
+                    c = self.state_avg_pool(c)
+
         x = self.avg_pool(x)
         x = self.flatten(x)
         return self.output(x)
@@ -152,10 +154,10 @@ class RegNet(pl.LightningModule):
 
 
 if __name__  == "__main__":
-    model = RegNet(3, 10)
+    model = RegNet(3, 10, 'lstm')
     output = model(torch.randn(4, 3, 224, 224))
     #print(output.shape)
-    trainer =  Trainer(fast_dev_run=False)
+    trainer =  Trainer(fast_dev_run=True)
     trainer.fit(model)
 
 
