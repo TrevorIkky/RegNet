61cded0697098888098cc665f495e4d9f3593e2d
ikky <ikramikky4462@gmail.com>
2021-11-27 12:36:57 +0300

    Defined search space for regnet blocks

--- regnet.py
+++ regnet.py
@@ -4,20 +4,25 @@ import torch.nn as nn
 import torchmetrics as tm
 import torch.nn.functional as F
 
+import argparse
+
 import pytorch_lightning as pl
 from conv_rnns import ConvGRUCell, ConvLSTMCell
 
 
 from torch.optim import Adam, SGD
-from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from pytorch_lightning.trainer import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from cifar10_datamodule import Cifar10DataModule
 
-learning_rate = 0.001
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler, PopulationBasedTraining
+from ray.tune.integration.pytorch_lightning import TuneReportCallback, TuneReportCheckpointCallback
+
 momentum = 0.9
-weight_decay = 1e-4
 max_epochs = 30
 batch_size = 64
 
@@ -72,7 +77,7 @@ class rnn_regulated_block(nn.Module):
         return c, h, self.relu(x)
 
 class RegNet(pl.LightningModule):
-    def __init__(self, in_dim:int, intermediate_channels:int, classes:int=3, cell_type:str='gru', layers:typing.List=[3, 4, 6, 3]):
+    def __init__(self, regulated_block:nn.Module, in_dim:int, intermediate_channels:int, classes:int=3, cell_type:str='gru', layers:typing.List=[3, 4, 6, 3], **kwargs):
         super(RegNet, self).__init__()
         self.layers = layers
         self.classes = classes
@@ -93,7 +98,7 @@ class RegNet(pl.LightningModule):
                 nn.BatchNorm2d(channels * 4)
             )
 
-            regulated_blocks.append(rnn_regulated_block(
+            regulated_blocks.append(regulated_block(
                 self.intermediate_channels, channels,
                 self.cell(channels,h_channels , kernel_size=3),
                 identity_block, stride
@@ -101,7 +106,7 @@ class RegNet(pl.LightningModule):
 
             for block in range(layers[layer] - 1):
                 self.intermediate_channels = channels * 4 if block < 1 else self.intermediate_channels
-                regulated_blocks.append(rnn_regulated_block(
+                regulated_blocks.append(regulated_block(
                         self.intermediate_channels, channels
                     )
                 )
@@ -116,6 +121,9 @@ class RegNet(pl.LightningModule):
         self.test_accuracy = tm.Accuracy()
         self.train_accuracy = tm.Accuracy()
 
+        self.config = {}
+        for key, val in kwargs.items():
+            self.config[key] = val
 
     def forward(self, x) -> torch.Tensor:
         x = self.conv1(x)
@@ -137,9 +145,16 @@ class RegNet(pl.LightningModule):
 
 
     def configure_optimizers(self):
-        optimizer= Adam(self.parameters(), lr=learning_rate)
-        lr_scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
-        return { "optimizer": optimizer, "lr_scheduler": lr_scheduler }
+        learning_rate = 0.1
+        weight_decay = 5e-4
+
+        if len(self.config.items()) > 0:
+            learning_rate = self.config['lr']
+            weight_decay = self.config['weight_decay']
+
+        optimizer= SGD(self.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
+        lr_scheduler = CosineAnnealingLR(optimizer, T_max=200)
+        return { "optimizer": optimizer, "lr_scheduler": lr_scheduler, "monitor":  "val_accuracy"}
 
     def training_step(self, batch, batch_idx):
         images, labels = batch
@@ -158,9 +173,11 @@ class RegNet(pl.LightningModule):
         loss = F.cross_entropy(outputs, labels)
         outputs = torch.argmax(outputs, dim=-1)
         accuracy = self.val_accuracy(outputs, labels)
-        return { "val_loss" : loss, "val_accuracy" : accuracy }
+        return { "val_loss" : loss }
 
     def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        self.log('val_loss', avg_loss)
         self.log('val_accuracy', self.val_accuracy, prog_bar=True)
 
 
@@ -170,24 +187,167 @@ class RegNet(pl.LightningModule):
         loss = F.cross_entropy(outputs, labels)
         outputs = torch.argmax(outputs, dim=-1)
         accuracy = self.test_accuracy(outputs, labels)
-        return { "test_loss" : loss, "test_accuracy" : accuracy}
+        return { "test_loss" : loss }
 
     def test_epoch_end(self, outputs):
         self.log('test_accuracy', self.test_accuracy, prog_bar=True)
 
-if __name__  == "__main__":
+
+def train_regnet(config, num_epochs=10, num_gpus=1):
+
+    block1 = config['block1']
+    block2 = config['block2']
+    block3 = config['block3']
+    block4 = config['block4']
+    layers = [block1, block2, block3, block4]
+
+    batch_size = config['batch_size']
+    intermediate_channels = config['intermediate_channels']
+    cell_type = config['cell_type']
+
     cfm = Cifar10DataModule(batch_size=batch_size)
-    model = RegNet(cfm.image_dims[0], 32, cfm.num_classes, 'gru')
+    model = RegNet(
+        rnn_regulated_block, cfm.image_dims[0], intermediate_channels,
+        classes=cfm.num_classes, cell_type=cell_type, layers=layers, config=config
+    )
+
+    ### Log metric progression
+    logger = TensorBoardLogger('tuner_logs', name='regnet_logs')
+
+
+    #Tune callback
+    tune_report = TuneReportCallback({ "loss": "val_loss", "val_accuracy": "val_accuracy"}, on="validation_end")
+    tune_report_ckpt = TuneReportCheckpointCallback(
+        metrics={ "val_loss": "val_loss", "val_accuracy": "val_accuracy"},
+        filename="tune_last_ckpt", on="validation_end"
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=num_epochs, gpus= num_gpus, logger=logger,
+        callbacks=[ tune_report, tune_report_ckpt ]
+    )
+    trainer.fit(model, cfm)
+
+
+#======================================= Tuning Functions ============================================
+
+def TuneAsha(train_fn, model:str, num_samples:int=10, num_epochs:int=10, cpus_per_trial:int=1, gpus_per_trial:int=1, data_dir='./tuner'):
+    config = {
+        "block1": tune.randint(2, 3),
+        "block2": tune.randint(2, 5),
+        "block3": tune.randint(2, 5),
+        "block4": tune.randint(2, 5),
+        "cell_type": tune.choice(['gru', 'lstm']),
+        "intermediate_channels": tune.choice([16, 32, 64]),
+        "lr": tune.loguniform(1e-4, 1e-1),
+        "batch_size": tune.choice([32, 64, 128]),
+        "weight_decay": tune.loguniform(1e-4, 1e-5),
+    }
+
+    scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
+
+    reporter = CLIReporter(
+        parameter_columns=[ "block1", "block2", "block3", "block4", "lr", "batch_size", "weight_decay"],
+        metric_columns=["val_loss", "val_accuracy", "training_iteration"]
+    )
+
+    analysis = tune.run(
+        tune.with_parameters(
+            train_fn, num_epochs=num_epochs,
+            num_gpus=gpus_per_trial
+        ),
+        resources_per_trial={
+            "cpu": cpus_per_trial,
+            "gpu": gpus_per_trial
+        },
+        metric="val_loss", mode="min", config=config,
+        num_samples=num_samples, scheduler=scheduler, progress_reporter=reporter, name=f"{model}_asha"
+    )
+    print("Best hyperparameters found were: ", analysis.best_config)
+    exit(0)
+
+def TunePBT(train_fn, model:str, num_samples:int=10, num_epochs:int=10, cpus_per_trial:int=1, gpus_per_trial:int=1, data_dir='./tuner'):
+    config = {
+        "block1": tune.randint(2, 3),
+        "block2": tune.randint(2, 5),
+        "block3": tune.randint(2, 5),
+        "block4": tune.randint(2, 5),
+        "cell_type": tune.choice(['gru', 'lstm']),
+        "intermediate_channels": tune.choice([16, 32, 64]),
+        "lr": tune.loguniform(1e-4, 1e-1),
+        "batch_size": tune.choice([32, 64, 128]),
+        "weight_decay": tune.loguniform(1e-4, 1e-5),
+    }
+
+    scheduler = PopulationBasedTraining(
+        perturbation_interval=4,
+        hyperparam_mutations={
+            "block1": tune.randint(2, 3),
+            "block2": tune.randint(2, 5),
+            "block3": tune.randint(2, 5),
+            "block4": tune.randint(2, 5),
+            "lr": tune.loguniform(1e-4, 1e-1),
+            "weight_decay": tune.loguniform(1e-4, 1e-5),
+            "batch_size": [32, 64, 128],
+            "intermediate_channels": [16, 32, 64],
+        }
+    )
+
+    reporter = CLIReporter(
+        parameter_columns=[ "block1", "block2", "block3", "block4", "lr", "batch_size", "weight_decay"],
+        metric_columns=["loss", "val_accuracy", "training_iteration"])
+
+    analysis = tune.run(
+        tune.with_parameters(
+            train_fn, num_epochs=num_epochs,
+            num_gpus=gpus_per_trial
+        ),
+        resources_per_trial={
+            "cpu": cpus_per_trial,
+            "gpu": gpus_per_trial
+        },
+        metric="val_loss", mode="min", config=config,
+        num_samples=num_samples, scheduler=scheduler, progress_reporter=reporter, name=f"{model}_asha"
+    )
+    print("Best hyperparameters found were: ", analysis.best_config)
+    exit(0)
+
+#====================================== End Tuning Functions =====================================
+
+
+
+if __name__  == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tune', help='Find hyperparameter values', action='store_true')
+    args = parser.parse_args()
+    if args.tune:
+        TunePBT(
+            train_regnet, 'regnet', num_samples=30, num_epochs=3,
+            cpus_per_trial=4, gpus_per_trial=1
+        )
+
+    cfm = Cifar10DataModule(batch_size=batch_size)
+    model = RegNet(rnn_regulated_block, cfm.image_dims[0], 32, cfm.num_classes, 'gru', [2, 2, 2, 2])
+
+
     ### Log metric progression
     logger = TensorBoardLogger('logs', name='regnet_logs')
 
     ### Callbacks
     stop_early = EarlyStopping(monitor='val_accuracy', patience=3)
+    last_chkpt_path = 'checkpoints/last.ckpt'
+    checkpoint = ModelCheckpoint(
+        dirpath= last_chkpt_path, monitor='val_accuracy',
+        filename='{epoch}-{val_accuracy:.2f}', verbose=True, save_top_k=1
+    )
+
 
     trainer = Trainer(
         gpus=0, fast_dev_run=True, logger=logger,
-        max_epochs=max_epochs, callbacks=[stop_early]
+        max_epochs=max_epochs, callbacks=[checkpoint],
     )
+
     trainer.fit(model, cfm)
 
 
